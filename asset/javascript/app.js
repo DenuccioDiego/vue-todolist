26e2602f25064d8ac66d33e8d11e487cf18a5f83
DenuccioDiego <diegodenuccio95@gmail.com>
2021-11-02 16:49:23 +0100

    prova bonus modifica task

--- asset/javascript/app.js
+++ asset/javascript/app.js
@@ -9,15 +9,15 @@ var app = new Vue({
           ],
           newTask:"",
           error: false,
+          mostra: false,
+          nascondi: true,
      },
+
      methods:  {
 
-          
-
-          
 
           removeTask(i){
-             console.log("cliccato rimuovi"),
+             //console.log("cliccato rimuovi"),
              this.tasks.splice(i, 1) 
           },
 
@@ -26,14 +26,28 @@ var app = new Vue({
                if(this.newTask.length > 5){
                     this.tasks.push(this.newTask)
                     this.error = false
-                   console.log(this.tasks)
+                    //console.log(this.tasks)
                }
                else{
                     this.error = true
                }
+               this.newTask = ""
+          },
+
+          changeTask(i){
+               //console.log(this.tasks[i])
+               if(this.mostra = true ){
+                    this.nascondi = false
+                    this.mostra = true 
+                    console.log(this.mostra)
+               }
+               else if(this.nascondi = false){
+                    this.nascondi = true
+                    this.mostra = false
+                    console.log(this.nascondi)
+               }
                
           }
-
      }
      
 })
