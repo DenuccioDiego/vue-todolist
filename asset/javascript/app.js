47b7d7595b973471e5be837a43cb0e72e1df384a
DenuccioDiego <diegodenuccio95@gmail.com>
2021-11-02 12:54:10 +0100

    tasto per aggiungere le task

--- asset/javascript/app.js
+++ asset/javascript/app.js
@@ -1,6 +1,26 @@
 var app = new Vue({
      el: '#app',
      data: {
-       message: 'Hello Vue!'
+          message: 'Hello Vue!',
+          tasks : [
+               "fare la spesa",
+               "studiare",
+               "mangiare",
+          ],
+          newTask:"",
+
+     },
+     methods:  {
+
+          removeTask(){
+               
+          },
+
+          addTask(){
+               console.log("aggiungo")
+               this.tasks.push(this.newTask)
+          }
+
      }
+     
 })
